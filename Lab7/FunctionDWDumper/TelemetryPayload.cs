9953603188fbfef7b81c0a2725e8282e8def61fe
shap0011 <shap0011@algonquinlive.com>
2026-03-07 21:53:36 -0500

    Lab7: Created Azure Functions project and telemetry model

---
+++ Lab7/FunctionDWDumper/TelemetryPayload.cs
@@ -0,0 +1,11 @@
+namespace FunctionDWDumper
+{
+    public class TelemetryPayload
+    {
+        public string DeviceId { get; set; }
+        public string Timestamp { get; set; }
+        public double WindSpeed { get; set; }
+        public double GeneratedPower { get; set; }
+        public double TurbineSpeed { get; set; }
+    }
+}
